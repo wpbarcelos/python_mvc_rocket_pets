086cf6d178a2ab97f231d578fdf1353a3dd9bdc4
Wanderson P. Barcelos <wp.barcelos@gmail.com>
2025-11-03 20:08:48 -0300

    feat: implementing the rest of the views

---
+++ src/views/pet_deleter_view.py
@@ -0,0 +1,14 @@
+from src.controllers.interfaces.pet_deleter_controller import PetDeleterControllerInterface
+from .http_types.http_request import HttpRequest
+from .http_types.http_response import HttpResponse
+from .interfaces.view_interface import ViewInterface
+
+
+class PetListerView(ViewInterface):
+    def __init__(self, controller: PetDeleterControllerInterface) -> None:
+        self.__controller = controller
+
+    def handle(self, http_request: HttpRequest) -> HttpResponse:
+        pet_name = http_request.param['name']
+        self.__controller.delete(pet_name)
+        return HttpResponse(status_code=204)
